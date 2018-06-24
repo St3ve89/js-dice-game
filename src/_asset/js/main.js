d3589e8af72165449e55e92858b9f094f72f25cb
St3ve89 <istvan.acs89@gmail.com>
2018-06-24 16:02:23 +0100

    Change the text and hide the dice

--- src/_asset/js/main.js
+++ src/_asset/js/main.js
@@ -4,4 +4,11 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
+// make a random number between 1 and 6
 dice = Math.floor(Math.random() * 6) + 1;
+// select a #current and change the text on it
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// hide the dice
+
+document.querySelector('.dice').style.display = 'none';
