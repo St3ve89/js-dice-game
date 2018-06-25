48d0faeb4e7e5fcb92f603bcc7868503976cd4ce
St3ve89 <istvan.acs89@gmail.com>
2018-06-25 20:57:37 +0100

    Add eventlisteners to the roll dice button

--- src/_asset/js/main.js
+++ src/_asset/js/main.js
@@ -1,14 +1,29 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-// make a random number between 1 and 6
-dice = Math.floor(Math.random() * 6) + 1;
+
 // select a #current and change the text on it
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 
 // hide the dice
-
 document.querySelector('.dice').style.display = 'none';
+// set everything to 0
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // make a random number between 1 and 6
+  var dice = Math.floor(Math.random() * 6) + 1;
+  // display the result
+  var diceDOM = document.querySelector('.dice');
+  // display back the dice
+  diceDOM.style.display = 'block';
+  // change the image of the dice
+  diceDOM.src = 'asset/image/dice-' + dice + '.png';
+  // update the round score if the rolled number was not a 1
+});
